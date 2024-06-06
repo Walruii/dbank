df4fb33d4fe384a7054d5abc85961148b64286ff
walruii <walruiix@gmail.com>
2024-06-06 12:49:25 +0530

    stuff

--- src/dbank_frontend/src/App.jsx
+++ src/dbank_frontend/src/App.jsx
@@ -1,29 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { dbank_backend } from 'declarations/dbank_backend';
 
-function App() {
-  const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    dbank_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+function App() {
+  const [balance, setBalance] = useState(100);
+  const updateBalance = async () => {
+    await dbank_backend.checkBalance().then((bal) => {
+      setBalance(bal);
     });
-    return false;
+  }
+  updateBalance();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const amountTopUp = parseFloat(e.target.elements.topUp.value);
+    const amountWith = parseFloat(e.target.elements.topUp.value);
+    await dbank_backend.topUp(amountTopUp);
+    await dbank_backend.botDown(amountWith);
+    await updateBalance();
   }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <div class="container">
+        <img src="dbank_logo.png" alt="DBank logo" width="100" />
+        <h1>Current Balance: $ {balance}</h1>
+        <div class="divider"></div>
+        <form onSubmit={handleSubmit}>
+          <h2>Amount to Top Up</h2>
+          <input id="input-amount" type="number" step="0.01" min={0} name="topUp" />
+          <h2>Amount to Withdraw</h2>
+          <input id="withdrawal-amount" type="number" name="withdraw" step="0.01" min={0} />
+          <input id="submit-btn" type="submit" value="Finalise Transaction" />
+        </form>
+      </div>
     </main>
   );
 }
